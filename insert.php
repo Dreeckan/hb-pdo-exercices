1780303a3f2aeb0a8d8e6165f75ec9dc8e8ed86a
Dekleor <67908404+Dekleor@users.noreply.github.com>
2021-01-27 16:26:33 +0100

    J'ai le truc !

--- insert.php
+++ insert.php
@@ -2,10 +2,32 @@
 require_once 'includes/autoload.php';
 require_once 'includes/connect.php';
 
-$sql = "INSERT INTO `computer`(`name`)
-        VALUES ('ASTUS Rogue One'), ('Sansong Galaxy Truc')";
-        $count = $connection->exec($sql);
+// $sql = "INSERT INTO `computer`(`name`)
+//         VALUES ('ASTUS Rogue One'), ('Sansong Galaxy Truc')";
+//         $count = $connection->exec($sql);
 
-$sql = "INSERT INTO `component`(`name`, `brand`)
-        VALUES ('Piece de la mort', 'ASTUS')";
-        $count = $connection->exec($sql);
+// $sql = "INSERT INTO `component`(`name`, `brand`)
+//         VALUES ('Piece de la mort', 'ASTUS')";
+//         $count = $connection->exec($sql);
+
+$sql = "INSERT INTO `component`(`name`, `brand`, `type`) VALUES (:name, :brand, :type)";
+for ($i = 0; $i < 20; $i++) {
+        $name = 'DBGT2077'.$i;
+        $brand = 'ASTUS'.$i;
+        $type = 'graphicCard'.$i;
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':name', $name, PDO::PARAM_STR);
+        $statement->bindParam(':brand', $brand, PDO::PARAM_STR);
+        $statement->bindParam(':type', $type, PDO::PARAM_STR);
+        $statement->execute();
+}
+
+$sql = "INSERT INTO `computer`(`name`, `type`) VALUES (:name, :type)";
+for ($i = 0; $i < 20; $i++) {
+        $name = 'Aliengear'.$i;
+        $type = 'Laptop'.$i;
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':name', $name, PDO::PARAM_STR);
+        $statement->bindParam(':type', $type, PDO::PARAM_STR);
+        $statement->execute();
+}
--- update.php
+++ update.php
@@ -15,17 +15,40 @@ require_once 'includes/connect.php';
 // ADD type VARCHAR(32) DEFAULT NULL";
 // $count = $connection->exec($sql);
 
-$sql = "UPDATE `computer`
-        SET type = 'desktop'
-        WHERE id = 13";
-        $count = $connection->exec($sql);
+// $sql = "UPDATE `computer`
+//         SET type = 'desktop'
+//         WHERE id = 13";
+//         $count = $connection->exec($sql);
 
-$sql = "UPDATE `computer`
-        SET type = 'laptop'
-        WHERE id = 14";
-        $count = $connection->exec($sql);
+// $sql = "UPDATE `computer`
+//         SET type = 'laptop'
+//         WHERE id = 14";
+//         $count = $connection->exec($sql);
 
-$sql = "UPDATE `component`
-        SET type = 'ram'
-        WHERE id = 2";
-        $count = $connection->exec($sql);
+// $sql = "UPDATE `component`
+//         SET type = 'ram'
+//         WHERE id = 2";
+//         $count = $connection->exec($sql);
+
+$sth = $connection->prepare("SELECT * FROM computer");
+$sth->execute();
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($result as $results) {
+    echo $results['id']; echo '<br />';
+    echo $results['name']; echo '<br />';
+    echo $results['type']; echo '<br />';
+    echo '<br />';
+}
+
+$sth = $connection->prepare("SELECT * FROM component");
+$sth->execute();
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($result as $results) {
+    echo $results['id']; echo '<br />';
+    echo $results['brand']; echo '<br />';
+    echo $results['name']; echo '<br />';
+    echo $results['type']; echo '<br />';
+    echo '<br />';
+}
